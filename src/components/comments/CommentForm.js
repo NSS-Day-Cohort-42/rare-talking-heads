7ce1fee129e7733df62155b1cd6a43f616f0196c
Mark Skinner <jmskinne@gmail.com>
2020-10-27 10:42:37 -0500

    wip

--- src/components/comments/CommentForm.js
+++ src/components/comments/CommentForm.js
@@ -8,6 +8,30 @@ export const CommentForm = (props) => {
 
     const [editMode, editModeChanged] = useState(false)
 
-    
+    const toEdit = props.match.params.hasOwnProperty("commentId")
+
+    useEffect(() => {
+        if(toEdit) {
+            editModeChanged(true)
+        } else {
+            editModeChanged(false)
+        }
+    }, [comment])
+
+    const handleChange = e => {
+        const newComment = Object.assign({}, comment)
+        newComment[e.target.name] = e.target.value
+        setComment(newComment)
+    }
+
+    const makeNewComment = () => {
+        if(editMode) {
+            console.log("Adding function next")
+        
+    } else {
+        createComment({
+            
+        })
+    }
 
 }
