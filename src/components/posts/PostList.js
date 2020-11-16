a7fae6e97dbb7fc21559168d2330260a031ab330
Pete Stewart <petestew@gmail.com>
2020-11-16 14:04:05 -0600

    wip

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,11 +1,14 @@
 import React, { useContext, useEffect, useState } from "react"
-
 import { Link } from "react-router-dom"
+
+import { ProfileContext } from "../auth/AuthProvider"
 import { PostContext } from "./PostProvider"
 import "./Posts.css"
 
 export const PostList = props => {
     const { posts, getAllPosts, getPostsByUser, myposts } = useContext(PostContext)
+    const { isAdmin } = useContext(ProfileContext)
+
 
     const [view, setView] = useState('all')
 
@@ -23,23 +26,11 @@ export const PostList = props => {
         else {
             getAllPosts()
         }
-    }, [])
+    }, []) 
 
-    // useEffect(() => {
-    //     getAllPosts()
-    // }, [])
-
-    // useEffect(() => {
-    //     const owner = posts.find(p => p.is_owner === true) || {}
-        
-    //         if (owner != undefined) {
-    //             getPostsByUser(owner.author_id)
-                
-    //     }
-        
-    // }, [posts])
-
-    
+    const approvalHandler = (e) => {
+        console.log(e.target.id)
+    };
 
     return (
         <div>
@@ -61,25 +52,42 @@ export const PostList = props => {
                                 return false
                             }
                         }
-                        return <section className = "post-preview" key={post.id}>
-                            <div className = "post-preview-header">
-                                By: {post.author.user.first_name} {post.author.user.last_name}
-                                {ableToEdit() ? (<span className="edit-button"> {/* If user id matches the post.user_id they will be able to edit post */}
-                                    <i className="fas fa-edit"
-                                    style={{cursor:'pointer'}}
-                                    onClick={() => {props.history.push(`/posts/edit/${post.id}`)}}>Edit</i>
-                                </span>) : <span className="edit-button"> </span>}
-                                
-                            </div>
-                            <div className="post-preview-title">
-                                <Link to={`/posts/${post.id}`}>
-                                    <h3>{post.title}</h3>
-                                </Link>
-                            </div>
-                            <div className = "post-preview-footer">
-                                Category: {post.category.label}
-                            </div>
-                        </section>
+                        return (
+                            <section className = "post-preview" key={post.id}>
+                                <div className = "post-preview-header">
+                                    By: {post.author.user.first_name} {post.author.user.last_name}
+                                    {ableToEdit() ? (<span className="edit-button"> {/* If user id matches the post.user_id they will be able to edit post */}
+                                        <i className="fas fa-edit"
+                                        style={{cursor:'pointer'}}
+                                        onClick={() => {props.history.push(`/posts/edit/${post.id}`)}}>Edit</i>
+                                    </span>) : <span className="edit-button"> </span>}
+                                    
+                                </div>
+                                <div className="post-preview-title">
+                                    <Link to={`/posts/${post.id}`}>
+                                        <h3>{post.title}</h3>
+                                    </Link>
+                                </div>
+                                <div className = "post-preview-footer d-flex justify-content-between">
+                                    <div>
+                                        { isAdmin
+                                            ?   <div className="form-check">
+                                                    <input class="form-check-input" type="checkbox" checked={post.approved} id={post.id} onChange={approvalHandler}/>
+                                                    <label class="form-check-label" for="defaultCheck1">
+                                                        Default checkbox
+                                                    </label>
+                                                
+                                                </div>
+                                            : ''
+                                        }
+                                    </div>
+                                    <div>
+                                        Category: {post.category.label}
+                                    </div>
+                                </div>
+                            </section>
+                            
+                        )
                     })
                     : myposts.map(post => {
                         // this function checks to see if the current user has any posts that they wrote
